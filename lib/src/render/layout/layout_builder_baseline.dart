bd30a5faca93fe1279f1d39d347f9dbe8472877b
Daksh Vasudev <136306675+dakshatsuraasa@users.noreply.github.com>
2025-06-10 17:19:51 +0530

    fix: support for flutter v3.32.2

--- lib/src/render/layout/layout_builder_baseline.dart
+++ lib/src/render/layout/layout_builder_baseline.dart
@@ -23,7 +23,6 @@ class LayoutBuilderPreserveBaseline
 class _RenderLayoutBuilderPreserveBaseline extends RenderBox
     with
         RenderObjectWithChildMixin<RenderBox>,
-        RenderObjectWithLayoutCallbackMixin,
         RenderConstrainedLayoutBuilder<BoxConstraints, RenderBox> {
   @override
   double? computeDistanceToActualBaseline(TextBaseline baseline) =>
@@ -60,7 +59,6 @@ class _RenderLayoutBuilderPreserveBaseline extends RenderBox
   @override
   void performLayout() {
     final constraints = this.constraints;
-    runLayoutCallback();
     if (child != null) {
       child!.layout(constraints, parentUsesSize: true);
       size = constraints.constrain(child!.size);
